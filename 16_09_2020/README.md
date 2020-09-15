5f92d031722aab17926e5b83b145d1b1f257ade1
User <you@example.com>
2020-09-15 19:27:46 +0500

    update

---
+++ 16_09_2020/README.md
@@ -0,0 +1,24 @@
+Полезные ссылки
+===
+
+### ПО
+
+Текстовые редакторы:
+- [sublime Текстовый редактор с подсветкой кода](https://www.sublimetext.com/3)
+- [VSCode](https://code.visualstudio.com/download)
+
+Интерпретатор python
+- [Скачать python](https://www.python.org/downloads/)
+
+Полноценная IDE
+- [PyCharm](https://www.jetbrains.com/ru-ru/pycharm/)
+
+Перед началом занятий установи
+> Либо текстовый редактор + python
+> Либо только IDE
+
+
+### Полезные ссылки:
+[Условный оператор](https://pythonworld.ru/osnovy/instrukciya-if-elif-else-proverka-istinnosti-trexmestnoe-vyrazhenie-ifelse.html)
+
+[Циклы](https://pythonworld.ru/osnovy/cikly-for-i-while-operatory-break-i-continue-volshebnoe-slovo-else.html)
---
+++ 16_09_2020/main.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python3
+
+
+# Переменные
+a = 10				# int
+b = 3.1415926		# float
+c = "Hello"			# string
+d = [1, 2, 3]		# array
+
+
+# ввод вывод
+my_str = input("Введите строку: ")
+print("Вы ввели строку: " + my_str)
+
+a = int(input("Введите значение А: "))
+print("Вы ввели число: " + str(a))
+
+# Ветвление
+if a % 2:
+	print("Это не четное число")
+else:
+	print("Это четное число")
+
+print("Вывод чисел:")
+print(a)
+print("Это A", a)
+print(f"Это A {a}")
+print("Это А {}".format(a))
+print("Это число А: " + str(a) + "!")
+
+
+print("--------- for -----------")
+for i in range(0, 5): # от 0 до 4
+	print(i)
+
+print("--------- for arr -----------")
+a = [1, 5, 2, 55, 22]
+for i in a:
+	print(i)
+
+print("-------- While ----------")
+
+a = 0
+while a < 4: # Вывод чисел от 0 до 4
+	print(a)
+	a = a + 1
+
+
+print("------- Функции ---------")
+
+
+def my_simple_func_sum(a, b):
+	# Функция подсчета суммы двух чисел
+	c = a + b
+	return c
+
+print(my_simple_func_sum(11, 2))
